7b985f075f3e0536b40d9f61b24aa0314a09bead
Eric Portis <e@ericportis.com>
2020-10-29 13:05:13 -0700

    Add the files

--- README.md
+++ README.md
@@ -1,2 +1,32 @@
 # exif-resolution
-A lil' script to scale images for the web using EXIF
+
+A lil’ script to scale images for the web using EXIF.
+
+Requires [exiftool](https://exiftool.org).
+
+## Usage
+
+### Target dimensions
+
+```
+% node exif-resolution img.jpg 400x300
+```
+
+No matter *what* `img.jpg`’s actual pixel dimensions are, this will set its [Exif][] metadata [in such a way][] that browsers[^some-browsers] will give it an [intrinsic size][] of 400-`px`-wide and 300-`px`-tall.
+
+
+### Target resolution
+
+```
+% node exif-resolution img.jpg 2x
+```
+You can also just give the script an image density. For example, if `img.jpg` is 800 × 600, this command is equivalent to the previous one. Both will cause browsers to give the image a [`.naturalWidth`](https://developer.mozilla.org/en-US/docs/Web/API/HTMLImageElement/naturalWidth) of 400, a [`.naturalHeight`](https://developer.mozilla.org/en-US/docs/Web/API/HTMLImageElement/naturalHeight) of 300, and an intrinsic density of 2x.
+
+---
+
+[^some-browsers]: Well, [some browsers](https://wpt.fyi/results/density-size-correction?label=experimental&label=master&aligned).
+
+[Exif]: https://en.wikipedia.org/wiki/Exif
+[intrinsic size]: https://developer.mozilla.org/en-US/docs/Glossary/Intrinsic_Size
+[in such a way]: https://github.com/whatwg/html/pull/5574
+[image density]: https://html.spec.whatwg.org/multipage/images.html#pixel-density-descriptor
---
+++ index.js
@@ -0,0 +1,82 @@
+#! /usr/local/bin/node
+
+const { execSync } = require( 'child_process' );
+
+const eg = `
+e.g.:
+node getResolution.js path/to/img.jpg 200x100
+`;
+
+const myArgs = process.argv.slice( 2 ),
+      pathToFile = myArgs[ 0 ];
+
+if ( !pathToFile ) {
+	throw "You must supply a file (and a resolution)." + eg;
+}
+
+// get the actual pixel dimensions of the image file
+const original = {};
+[ original.w, original.h ] = 
+	execSync( `exiftool -s -s -s -ImageSize ${ pathToFile }` )
+		.toString()
+		.replace( '\n', '' )
+		.split( 'x' )
+		.map( str => parseInt( str ) );
+
+const resolution = myArgs[ 1 ];
+
+if ( !resolution ) {
+	throw "You must supply a resolution.\n\ne.g." + eg;
+}
+
+const regexedResolution = resolution.match( /^([\d.]+)x(\d+)?$/ );
+
+if ( !regexedResolution ) {
+	throw "Resolution must either be in the format [WIDTH]x[HEIGHT] or [DPR]x";
+}
+
+// derive the desired EXIF-modified dimensions from input arguments
+const modified = {};
+if ( regexedResolution[ 2 ] ) { // we got [WIDTH]x[HEIGHT]
+
+	modified.w = parseInt( regexedResolution[ 1 ] );
+	modified.h = parseInt( regexedResolution[ 2 ] );
+
+} else { // we got [DPR]x
+
+	const dpr = parseFloat( regexedResolution[ 1 ] );
+	modified.w = Math.round( original.w / dpr );
+	modified.h = Math.round( original.h / dpr );
+
+}
+
+// calculate the resolution arguments
+// which are rationals, as strings...
+const xResolution = reduce( 72 * original.w, modified.w ).join( '/' ),
+      yResolution = reduce( 72 * original.h, modified.h ).join( '/' );
+
+// the business
+
+const command = `exiftool \\
+  -ResolutionUnit=inches \\
+  -XResolution=${ xResolution } \\
+  -YResolution=${ yResolution } \\
+  -ExifImageWidth=${ modified.w } \\
+  -ExifImageHeight=${ modified.h } \\
+  ${ pathToFile }`;
+console.log( command );
+console.log( execSync( command ).toString() );
+
+
+// utilities
+
+// Reduce a fraction by finding the Greatest Common Divisor and dividing by it.
+// https://stackoverflow.com/questions/4652468/is-there-a-javascript-function-that-reduces-a-fraction
+function reduce(numerator,denominator){
+  var gcd = function gcd(a,b){
+    return b ? gcd(b, a%b) : a;
+  };
+  gcd = gcd(numerator,denominator);
+  return [numerator/gcd, denominator/gcd];
+}
+
